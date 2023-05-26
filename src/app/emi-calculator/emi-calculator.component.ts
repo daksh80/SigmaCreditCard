2d67b3c0688a2e4fed1922ae0bf0abf52d8e99df
Dakshendra Singh <pal.dakshendra80@gmail.com>
2023-05-26 16:09:30 +0530

    updated code

--- src/app/emi-calculator/emi-calculator.component.ts
+++ src/app/emi-calculator/emi-calculator.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-emi-calculator',
@@ -8,25 +8,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class EmiCalculatorComponent implements OnInit {
   emiCalculate!: FormGroup;
+  emi: number = 0; // Assign an initial value to the 'emi' property
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.emiCalculate = this.fb.group({
-      CClimit: ['', Validators.required],
-      CCSpend: ['', Validators.required],
-      CCtenure: ['', Validators.required],
-      CCItyp: ['', Validators.required],
-      CCIntr: ['', Validators.required],
-      CCMY: ['', Validators.required],
-      CCType: ['', Validators.required]
+      CClimit: [0],
+      CCSpend: [0],
+      CCtenure: [0],
+      CCItyp: [0],
+      CCIntr: [''],
+      CCMY: [''],
+      CCType: ['']
     });
   }
 
   updateLimitValue(event: Event): void {
-  const value = (event.target as HTMLInputElement).value;
-  this.emiCalculate.patchValue({ CClimit: value });
-}
+    const value = (event.target as HTMLInputElement).value;
+    this.emiCalculate.patchValue({ CClimit: value });
+  }
 
   updateSpendValue(event: Event): void {
     const value = (event.target as HTMLInputElement).value;
@@ -44,6 +45,22 @@ export class EmiCalculatorComponent implements OnInit {
   }
 
   emiCalculategrp(): void {
-    // Handle the form submission logic here
+    const CClimit = this.emiCalculate.get('CClimit')?.value;
+    const CCSpend = this.emiCalculate.get('CCSpend')?.value;
+    const CCtenure = this.emiCalculate.get('CCtenure')?.value;
+    const CCItyp = this.emiCalculate.get('CCItyp')?.value;
+    const CCIntr = this.emiCalculate.get('CCIntr')?.value;
+    const CCMY = this.emiCalculate.get('CCMY')?.value;
+    const CCType = this.emiCalculate.get('CCType')?.value;
+
+    const principal = CCSpend - CClimit;
+    const rateOfInterest = CCIntr / 100;
+    const timePeriod = CCtenure;
+
+    const emi = (principal * rateOfInterest * Math.pow(1 + rateOfInterest, timePeriod)) /
+      (Math.pow(1 + rateOfInterest, timePeriod) - 1);
+
+    this.emi = emi;
+    console.log(this.emi)
   }
 }
