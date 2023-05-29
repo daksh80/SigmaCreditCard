41aa1d84ce9d9a62fbef4bd2f136ca615aace80f
Dakshendra Singh <pal.dakshendra80@gmail.com>
2023-05-29 13:57:31 +0530

    updated emi calculator part

--- src/app/emi-calculator/emi-calculator.component.ts
+++ src/app/emi-calculator/emi-calculator.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { creditcard } from 'creditcard.interface';
+import Highcharts from 'highcharts';
+import { ToastrService } from 'ngx-toastr';
+import { SharedService } from 'src/shared.service';
 
 @Component({
   selector: 'app-emi-calculator',
@@ -7,60 +12,110 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./emi-calculator.component.css']
 })
 export class EmiCalculatorComponent implements OnInit {
-  emiCalculate!: FormGroup;
-  emi: number = 0; // Assign an initial value to the 'emi' property
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private sharedService: SharedService,
+    private toastr: ToastrService
+  ) {}
 
-  constructor(private fb: FormBuilder) {}
+  uid: string | undefined;
+  selectedUserCreditCard: creditcard[] | undefined;
+  highcharts = Highcharts;
+  disabled = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  loanAmount = 0;
+  rateOfInterest = 0;
+  loanTerm = 0;
 
   ngOnInit(): void {
-    this.emiCalculate = this.fb.group({
-      CClimit: [0],
-      CCSpend: [0],
-      CCtenure: [0],
-      CCItyp: [0],
-      CCIntr: [''],
-      CCMY: [''],
-      CCType: ['']
-    });
+    this.uid = this.route.snapshot.paramMap?.get("uid") || "";
+    this.sharedService
+      .getUserCreditCard(this.uid)
+      .subscribe((creditCards: creditcard[]) => {
+        this.selectedUserCreditCard = creditCards;
+        console.log("hellloooo",this.selectedUserCreditCard);
+        this.updateChart();
+      });
+  }
+  updateChart() {
+    const chart = Highcharts.chart("loanChart", {
+      chart: {
+        type: "pie",
+      },
+      title: {
+        text: "Loan Details",
+      },
+      tooltip: {
+        valueSuffix: "",
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: "pointer",
+          dataLabels: {
+            enabled: true,
+            format: "{point.name}: {y}",
+          },
+          showInLegend: true,
+        },
+      },
+      series: [
+        {
+          name: "Value",
+          colorByPoint: true,
+          type: "pie",
+          innerSize: "75%",
+          data: [],
+        },
+      ],
+    } as any);
+
+    const interest = this.loanAmount * (this.rateOfInterest / 100);
+    const totalAmount = this.loanAmount + interest;
+    const monthlyInstallment = totalAmount / (this.loanTerm * 12);
+
+    chart.series[0].setData([
+      { name: "Loan Amount", y: this.loanAmount },
+      { name: "Rate of Interest", y: this.rateOfInterest },
+      { name: "Loan Term", y: this.loanTerm },
+      { name: "Total Interest", y: interest },
+      { name: "Total Amount", y: totalAmount },
+      { name: "Monthly Installment", y: monthlyInstallment },
+    ]);
   }
 
-  updateLimitValue(event: Event): void {
-    const value = (event.target as HTMLInputElement).value;
-    this.emiCalculate.patchValue({ CClimit: value });
+  
+
+  calculateLoan() {
+    const interest = this.loanAmount * (this.rateOfInterest / 100);
+    const totalAmount = this.loanAmount + interest;
+    const monthlyInstallment = totalAmount / (this.loanTerm * 12);
+
+    console.log("Interest:", interest);
+    console.log("Total Amount:", totalAmount);
+    console.log("Monthly Installment:", monthlyInstallment);
+    this.updateChart();
+  }
+  calculateMonthlyInstallment() {
+    const interest = this.loanAmount * (this.rateOfInterest / 100);
+    const totalAmount = this.loanAmount + interest;
+    const monthlyInstallment = totalAmount / (this.loanTerm * 12);
+    return monthlyInstallment.toFixed(2);
   }
 
-  updateSpendValue(event: Event): void {
-    const value = (event.target as HTMLInputElement).value;
-    this.emiCalculate.patchValue({ CCSpend: value });
+  calculateTotalAmount() {
+    const interest = this.loanAmount * (this.rateOfInterest / 100);
+    const totalAmount = this.loanAmount + interest;
+    return totalAmount.toFixed(2);
   }
 
-  updateTenureValue(event: Event): void {
-    const value = (event.target as HTMLInputElement).value;
-    this.emiCalculate.patchValue({ CCtenure: value });
-  }
-
-  updateInterestTypeValue(event: Event): void {
-    const value = (event.target as HTMLInputElement).value;
-    this.emiCalculate.patchValue({ CCItyp: value });
-  }
-
-  emiCalculategrp(): void {
-    const CClimit = this.emiCalculate.get('CClimit')?.value;
-    const CCSpend = this.emiCalculate.get('CCSpend')?.value;
-    const CCtenure = this.emiCalculate.get('CCtenure')?.value;
-    const CCItyp = this.emiCalculate.get('CCItyp')?.value;
-    const CCIntr = this.emiCalculate.get('CCIntr')?.value;
-    const CCMY = this.emiCalculate.get('CCMY')?.value;
-    const CCType = this.emiCalculate.get('CCType')?.value;
-
-    const principal = CCSpend - CClimit;
-    const rateOfInterest = CCIntr / 100;
-    const timePeriod = CCtenure;
-
-    const emi = (principal * rateOfInterest * Math.pow(1 + rateOfInterest, timePeriod)) /
-      (Math.pow(1 + rateOfInterest, timePeriod) - 1);
-
-    this.emi = emi;
-    console.log(this.emi)
+  calculateInterest() {
+    const interest = this.loanAmount * (this.rateOfInterest / 100);
+    return interest.toFixed(2);
   }
 }
